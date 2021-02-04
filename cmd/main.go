538d3f05f5d16d934326496ee18efb12df166296
drkchiloll <sam.womack@entropicos.com>
2021-02-04 07:57:15 -0600

    make the example barebones

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -19,24 +18,10 @@ func init() {
 }
 
 func main() {
-	wlc := aireos.New(host, user, pass)
+	wlc := aireos.New(host, user, pass, "old")
 	err := wlc.Client.Connect(3)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 	defer wlc.Logout()
-	ap, _ := wlc.GetAp("f0:b2:e5:c2:39:98")
-	fmt.Println(ap)
-	// apCdp := wlc.GetApCdp("ap01.it.corp-austin.tx")
-	// fmt.Println(apCdp)
-	// var wg1 sync.WaitGroup
-	// wg1.Add(1)
-	// go func() {
-	// 	aps, _ := wlc.GetApDb()
-	// 	for _, ap := range aps {
-	// 		fmt.Println(ap)
-	// 	}
-	// 	wg1.Done()
-	// }()
-	// wg1.Wait()
 }
